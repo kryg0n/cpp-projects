fb86e93e3109528aca76d6d2f3890c49b3a875eb
kryg0n <kryg0n.me@gmail.com>
2026-02-05 00:45:30 +0800

    hello

---
+++ Basics/hello.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+int main() {
+    // For loop
+    for (int i = 0; i < 5; i++) {
+        std::cout << "Iteration " << i << std::endl;
+    }
+    
+    // While loop
+    int j = 0;
+    while (j < 5) {
+        std::cout << "While " << j << std::endl;
+        j++;
+    }
+    
+    return 0;
+}
